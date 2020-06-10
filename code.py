7fc62989a3643ac7bbccf5fede97df09d15e664c
vetkan <bandarupallivenkat@yahoo.com>
2020-06-10 15:03:42 -0500

    final code

--- code.py
+++ code.py
@@ -1,9 +1,10 @@
-start = 'y'
-while start == 'y':
-    history = []
+start = 'c'
+history = []
+while start == 'c':
+    
     value = input('enter the inputs: ')
 
-    x = value.replace('+','').replace('-','').replace('/','').replace('*','').replace('%','').replace('^','')
+    x = value.replace('+','').replace('-','').replace('/','').replace('*','').replace('%','').replace('^','').replace(' ','')
     if x.isdigit():
         outvalues = value.replace(' ','*').replace('^','**')
         output = eval(outvalues)
@@ -16,8 +17,16 @@ while start == 'y':
     
     else:
         print('Invalid Inputs')
-    start = input('Do you want to perform another calculation (Y/N): ')
-    if start.lower() == 'n':
+    start = input('Do you want to perform another Calculation/ History/ Quit (first letter of option - c/h/q): ')
+    if start.lower() == 'h':
+        reverse_history = reversed(history)
+        for i in reverse_history:
+            print(str(i[0])+' - '+str(i[1]))
+        start = input('Do you want to perform another Calculation/ History/ Quit (first letter of option - c/h/q): ')
+    elif startlower() == 'c':
+        pass
+    elif start.lower() == 'q':
         break
-        
-        
+    else:
+        start = input('Do you want to perform another Calculation/ History/ Quit (first letter of option - c/h/q): ')
+
